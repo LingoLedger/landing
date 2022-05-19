bfd02a02d029e7f2be8ddefaae7c91d7a1721322
Lachlan Brooks <lachlan.brooks@crestrock.com.au>
2022-05-19 19:44:57 +1000

    hero section

--- src/components/sections/HeroSplit.js
+++ src/components/sections/HeroSplit.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import classNames from 'classnames';
-import { SectionSplitProps } from '../../utils/SectionProps';
-import Button from '../elements/Button';
-import Image from '../elements/Image';
+import React from "react";
+import classNames from "classnames";
+import { SectionSplitProps } from "../../utils/SectionProps";
+import Button from "../elements/Button";
+import Image from "../elements/Image";
 
 const propTypes = {
-  ...SectionSplitProps.types
-}
+  ...SectionSplitProps.types,
+};
 
 const defaultProps = {
-  ...SectionSplitProps.defaults
-}
+  ...SectionSplitProps.defaults,
+};
+
+const logos = [
+  "lever",
+  "peopleai",
+  "lattice",
+  "webflow",
+  "coinbase",
+  "alloy",
+  "contactout",
+  "ycombinator",
+];
 
 class HeroSplit extends React.Component {
-
   render() {
-
     const {
       className,
       topOuterDivider,
-      bottomOuterDivider,      
+      bottomOuterDivider,
       topDivider,
       bottomDivider,
       hasBgColor,
@@ -32,57 +41,85 @@ class HeroSplit extends React.Component {
     } = this.props;
 
     const outerClasses = classNames(
-      'hero section',
-      topOuterDivider && 'has-top-divider',
-      bottomOuterDivider && 'has-bottom-divider',
-      hasBgColor && 'has-bg-color',
-      invertColor && 'invert-color',
-      className
+      "hero section",
+      topOuterDivider && "has-top-divider",
+      bottomOuterDivider && "has-bottom-divider",
+      hasBgColor && "has-bg-color",
+      invertColor && "invert-color",
+      className,
     );
 
     const innerClasses = classNames(
-      'hero-inner section-inner',
-      topDivider && 'has-top-divider',
-      bottomDivider && 'has-bottom-divider'
+      "hero-inner section-inner",
+      topDivider && "has-top-divider",
+      bottomDivider && "has-bottom-divider",
     );
 
     const splitClasses = classNames(
-      'split-wrap',
-      invertMobile && 'invert-mobile',
-      invertDesktop && 'invert-desktop',
-      alignTop && 'align-top'
+      "split-wrap",
+      invertMobile && "invert-mobile",
+      invertDesktop && "invert-desktop",
+      alignTop && "align-top",
     );
 
     return (
-      <section
-        {...props}
-        className={outerClasses}
-      >
+      <section {...props} className={outerClasses}>
         <div className="container">
           <div className={innerClasses}>
             <div className={splitClasses}>
-              <div className="split-item">
+              <div className="split-item" style={{ marginBottom: 0 }}>
                 <div className="hero-content split-item-content center-content-mobile">
                   <h1 className="mt-0 mb-16 reveal-from-left">
-                    Landing template for startups
+                    The world’s largest marketplace to find Accredited
+                    Interpreters and Translators
+                    <span aria-label="" role={"img"}>
+                      ✨
+                    </span>
                   </h1>
-                  <p className="mt-0 mb-24 reveal-from-left" data-reveal-delay="200">
-                    Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
+                  <p
+                    className="mt-0 mb-24 reveal-from-left"
+                    data-reveal-delay="200"
+                  >
+                    Get your Legal Interpretation and Translation needs met,
+                    wherever and whenever. No hidden fees.
                   </p>
                   <div className="reveal-from-left" data-reveal-delay="400">
-                    <Button tag="a" color="primary" href="https://cruip.com/" wideMobile>
-                      Get started
+                    <Button
+                      tag="a"
+                      color="primary"
+                      href="https://cruip.com/"
+                      wideMobile
+                    >
+                      Book Now
                     </Button>
                   </div>
                 </div>
-                <div className="hero-figure is-3d split-item-image illustration-element-01 reveal-scale-up">
+                <div className="hero-figure">
                   <Image
-                    className="has-shadow"
-                    src={require('./../../assets/images/hero-image.svg')}
+                    src={require("./../../assets/images/hero-image.png")}
                     alt="Hero"
-                    width={528}
-                    height={396} />
+                    style={{ objectFit: "scale-down", boxShadow: "none" }}
+                  />
                 </div>
+              </div>
+              <div style={{ display: "flex", flexWrap: "wrap", gap: 32 }}>
+                {logos.map((logo, index) => (
+                  <div
+                    style={{
+                      height: 30,
+                      width: 120,
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                    key={index}
+                  >
+                    <Image
+                      style={{ objectFit: "contain" }}
+                      alt={logo}
+                      src={require(`./../../assets/images/logo-${logo}.png`)}
+                    />
+                  </div>
+                ))}
               </div>
             </div>
           </div>
