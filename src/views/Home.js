d92908857f32e3462e7ca2d20024345e215a9dca
Lachlan Brooks <lachlan.brooks@crestrock.com.au>
2022-05-19 20:01:51 +1000

    homepage

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import HeroSplit from '../components/sections/HeroSplit';
-import FeaturesSplit from '../components/sections/FeaturesSplit';
-import FeaturesTiles from '../components/sections/FeaturesTiles';
-import Pricing from '../components/sections/Pricing';
-import Cta from '../components/sections/Cta';
+import React from "react";
+import HeroSplit from "../components/sections/HeroSplit";
+import FeaturesSplit from "../components/sections/FeaturesSplit";
+import VideoFull from "../components/sections/VideoFull";
+// import Testimonials from "../components/sections/Testimonial";
 
 class Home extends React.Component {
   render() {
     return (
       <React.Fragment>
         <HeroSplit imageFill className="illustration-section-01" />
-        <FeaturesTiles />
-        <FeaturesSplit invertMobile imageFill className="illustration-section-05" />
-        <Pricing pricingSwitcher className="illustration-section-03" />
-        <Cta topDivider bottomDivider split />
+        <VideoFull />
+        <FeaturesSplit />
+        {/* <Testimonials /> */}
       </React.Fragment>
     );
   }
