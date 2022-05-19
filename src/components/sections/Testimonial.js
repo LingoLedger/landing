95b8c2b69f4d2de5fcd0eae6bd0dfd80f733d5d5
Lachlan Brooks <lachlan.brooks@crestrock.com.au>
2022-05-19 20:01:46 +1000

    WIP testimonial section

--- src/components/sections/Testimonial.js
+++ src/components/sections/Testimonial.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import classNames from 'classnames';
-import { SectionTilesProps } from '../../utils/SectionProps';
-import SectionHeader from './partials/SectionHeader';
-import Image from '../elements/Image';
+import React from "react";
+import classNames from "classnames";
+import { SectionTilesProps } from "../../utils/SectionProps";
+import SectionHeader from "./partials/SectionHeader";
+import Image from "../elements/Image";
 
 const propTypes = {
-  ...SectionTilesProps.types
-}
+  ...SectionTilesProps.types,
+};
 
 const defaultProps = {
-  ...SectionTilesProps.defaults
-}
+  ...SectionTilesProps.defaults,
+};
 
 class Testimonial extends React.Component {
-
   render() {
-
     const {
       className,
       topOuterDivider,
-      bottomOuterDivider,      
+      bottomOuterDivider,
       topDivider,
       bottomDivider,
       hasBgColor,
@@ -29,53 +27,75 @@ class Testimonial extends React.Component {
     } = this.props;
 
     const outerClasses = classNames(
-      'testimonial section',
-      topOuterDivider && 'has-top-divider',
-      bottomOuterDivider && 'has-bottom-divider',
-      hasBgColor && 'has-bg-color',
-      invertColor && 'invert-color',
-      className
+      "testimonial section",
+      topOuterDivider && "has-top-divider",
+      bottomOuterDivider && "has-bottom-divider",
+      hasBgColor && "has-bg-color",
+      invertColor && "invert-color",
+      className,
     );
 
     const innerClasses = classNames(
-      'testimonial-inner section-inner',
-      topDivider && 'has-top-divider',
-      bottomDivider && 'has-bottom-divider'
+      "testimonial-inner section-inner",
+      topDivider && "has-top-divider",
+      bottomDivider && "has-bottom-divider",
     );
 
     const tilesClasses = classNames(
-      'tiles-wrap illustration-section-06',
-      pushLeft && 'push-left'
+      "tiles-wrap illustration-section-06",
+      pushLeft && "push-left",
     );
 
     const sectionHeader = {
-      title: 'Testimonials - Lorem ipsum is placeholder text commonly used.',
-      paragraph: ''
+      title: "Testimonials - Lorem ipsum is placeholder text commonly used.",
+      paragraph: "",
     };
 
     return (
-      <section
-        {...props}
-        className={outerClasses}
-      >
+      <section {...props} className={outerClasses}>
         <div className="container">
           <div className={innerClasses}>
-            <SectionHeader data={sectionHeader} className="center-content invert-color" />
-            <div className={tilesClasses}>
+            <SectionHeader
+              data={sectionHeader}
+              className="center-content invert-color"
+            />
+            <div className="split-item reveal-scale-up">
+              <div className="split-item-content center-content-mobile">
+                <h3 className="mt-0 mb-16">
+                  Rated 5/5 stars in over 1,000 reviews
+                </h3>
+                <p className="m-0">
+                  Find out why thousands of organisations, language experts, and
+                  immigrants love Lingo Ledger
+                </p>
+              </div>
+              <div>
+                <Image
+                  src={require("./../../assets/images/hero-image.png")}
+                  alt="Features split 03"
+                />
+              </div>
+            </div>
 
+            <div className={tilesClasses}>
               <div className="tiles-item reveal-scale-up">
                 <div className="tiles-item-inner has-shadow">
                   <div className="testimonial-item-header">
                     <div className="testimonial-item-image">
                       <Image
-                        src={require('./../../assets/images/testimonial-image-01.png')}
+                        src={require("./../../assets/images/testimonial-image-01.png")}
                         alt="Testimonial 01"
                         width={56}
-                        height={56} />
+                        height={56}
+                      />
                     </div>
                     <div className="testimonial-item-content">
                       <p className="text-sm mb-0">
-                        — Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum cillum dolore eu fugiat.
+                        — Duis aute irure dolor in reprehenderit in voluptate
+                        velit esse cillum dolore eu fugiat nulla pariatur.
+                        Excepteur sint occaecat cupidatat non proident, sunt in
+                        culpa qui officia deserunt mollit anim id est laborum
+                        cillum dolore eu fugiat.
                       </p>
                     </div>
                     <div className="testimonial-item-footer h6 mt-32 mb-0 pt-24 has-top-divider">
@@ -91,19 +111,27 @@ class Testimonial extends React.Component {
                 </div>
               </div>
 
-              <div className="tiles-item reveal-scale-up" data-reveal-delay="200">
+              <div
+                className="tiles-item reveal-scale-up"
+                data-reveal-delay="200"
+              >
                 <div className="tiles-item-inner has-shadow">
                   <div className="testimonial-item-header">
                     <div className="testimonial-item-image">
                       <Image
-                        src={require('./../../assets/images/testimonial-image-02.png')}
+                        src={require("./../../assets/images/testimonial-image-02.png")}
                         alt="Testimonial 02"
                         width={56}
-                        height={56} />
+                        height={56}
+                      />
                     </div>
                     <div className="testimonial-item-content">
                       <p className="text-sm mb-0">
-                        — Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum cillum dolore eu fugiat.
+                        — Duis aute irure dolor in reprehenderit in voluptate
+                        velit esse cillum dolore eu fugiat nulla pariatur.
+                        Excepteur sint occaecat cupidatat non proident, sunt in
+                        culpa qui officia deserunt mollit anim id est laborum
+                        cillum dolore eu fugiat.
                       </p>
                     </div>
                     <div className="testimonial-item-footer h6 mt-32 mb-0 pt-24 has-top-divider">
@@ -119,19 +147,27 @@ class Testimonial extends React.Component {
                 </div>
               </div>
 
-              <div className="tiles-item reveal-scale-up" data-reveal-delay="400">
+              <div
+                className="tiles-item reveal-scale-up"
+                data-reveal-delay="400"
+              >
                 <div className="tiles-item-inner has-shadow">
                   <div className="testimonial-item-header">
                     <div className="testimonial-item-image">
                       <Image
-                        src={require('./../../assets/images/testimonial-image-03.png')}
+                        src={require("./../../assets/images/testimonial-image-03.png")}
                         alt="Testimonial 03"
                         width={56}
-                        height={56} />
+                        height={56}
+                      />
                     </div>
                     <div className="testimonial-item-content">
                       <p className="text-sm mb-0">
-                        — Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum cillum dolore eu fugiat.
+                        — Duis aute irure dolor in reprehenderit in voluptate
+                        velit esse cillum dolore eu fugiat nulla pariatur.
+                        Excepteur sint occaecat cupidatat non proident, sunt in
+                        culpa qui officia deserunt mollit anim id est laborum
+                        cillum dolore eu fugiat.
                       </p>
                     </div>
                     <div className="testimonial-item-footer h6 mt-32 mb-0 pt-24 has-top-divider">
@@ -145,8 +181,7 @@ class Testimonial extends React.Component {
                     </div>
                   </div>
                 </div>
-              </div>                            
-
+              </div>
             </div>
           </div>
         </div>
@@ -157,5 +192,5 @@ class Testimonial extends React.Component {
 
 Testimonial.propTypes = propTypes;
 Testimonial.defaultProps = defaultProps;
-    
+
 export default Testimonial;
