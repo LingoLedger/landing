bd63eca8e9c05572dc7ba740326f4c5c3fed21d9
Lachlan Brooks <lachlan.brooks@crestrock.com.au>
2022-05-20 22:16:11 +1000

    reimplement testimonials

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import HeroSplit from "../components/sections/HeroSplit";
 import FeaturesSplit from "../components/sections/FeaturesSplit";
 import VideoFull from "../components/sections/VideoFull";
-// import Testimonials from "../components/sections/Testimonial";
+import Testimonials from "../components/sections/Testimonial";
 
 class Home extends React.Component {
   render() {
@@ -11,7 +11,7 @@ class Home extends React.Component {
         <HeroSplit imageFill className="illustration-section-01" />
         <VideoFull />
         <FeaturesSplit />
-        {/* <Testimonials /> */}
+        <Testimonials />
       </React.Fragment>
     );
   }
