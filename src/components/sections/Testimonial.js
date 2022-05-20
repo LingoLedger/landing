36bd96c0c8a490c8f62656f98171b82d322aaf40
Lachlan Brooks <lachlan.brooks@crestrock.com.au>
2022-05-20 22:20:16 +1000

    start on testimonial section

--- src/components/sections/Testimonial.js
+++ src/components/sections/Testimonial.js
@@ -41,10 +41,7 @@ class Testimonial extends React.Component {
       bottomDivider && "has-bottom-divider",
     );
 
-    const tilesClasses = classNames(
-      "tiles-wrap illustration-section-06",
-      pushLeft && "push-left",
-    );
+    const tilesClasses = classNames("tiles-wrapper illustration-section-06");
 
     const sectionHeader = {
       title: "Testimonials - Lorem ipsum is placeholder text commonly used.",
@@ -75,113 +72,26 @@ class Testimonial extends React.Component {
                   alt="Features split 03"
                 />
               </div>
-            </div>
-
-            <div className={tilesClasses}>
-              <div className="tiles-item reveal-scale-up">
-                <div className="tiles-item-inner has-shadow">
-                  <div className="testimonial-item-header">
-                    <div className="testimonial-item-image">
+              <ul className={tilesClasses}>
+                <li>
+                  <div style={{ display: "flex", gap: 8 }}>
+                    <div
+                      style={{
+                        display: "flex",
+                        marginBottom: "10px",
+                        flexDirection: "row",
+                        alignItems: "center",
+                      }}
+                    >
                       <Image
+                        style={{ borderRadius: "500px" }}
                         src={require("./../../assets/images/testimonial-image-01.png")}
-                        alt="Testimonial 01"
-                        width={56}
-                        height={56}
+                        width={40}
                       />
                     </div>
-                    <div className="testimonial-item-content">
-                      <p className="text-sm mb-0">
-                        — Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur.
-                        Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum
-                        cillum dolore eu fugiat.
-                      </p>
-                    </div>
-                    <div className="testimonial-item-footer h6 mt-32 mb-0 pt-24 has-top-divider">
-                      <span className="testimonial-item-name">
-                        Erika Muliawan
-                      </span>
-                      <span className="text-color-low"> - </span>
-                      <span className="testimonial-item-link text-xs">
-                        <a href="#0">AppName</a>
-                      </span>
-                    </div>
                   </div>
-                </div>
-              </div>
-
-              <div
-                className="tiles-item reveal-scale-up"
-                data-reveal-delay="200"
-              >
-                <div className="tiles-item-inner has-shadow">
-                  <div className="testimonial-item-header">
-                    <div className="testimonial-item-image">
-                      <Image
-                        src={require("./../../assets/images/testimonial-image-02.png")}
-                        alt="Testimonial 02"
-                        width={56}
-                        height={56}
-                      />
-                    </div>
-                    <div className="testimonial-item-content">
-                      <p className="text-sm mb-0">
-                        — Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur.
-                        Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum
-                        cillum dolore eu fugiat.
-                      </p>
-                    </div>
-                    <div className="testimonial-item-footer h6 mt-32 mb-0 pt-24 has-top-divider">
-                      <span className="testimonial-item-name">
-                        Erika Muliawan
-                      </span>
-                      <span className="text-color-low"> - </span>
-                      <span className="testimonial-item-link text-xs">
-                        <a href="#0">AppName</a>
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div
-                className="tiles-item reveal-scale-up"
-                data-reveal-delay="400"
-              >
-                <div className="tiles-item-inner has-shadow">
-                  <div className="testimonial-item-header">
-                    <div className="testimonial-item-image">
-                      <Image
-                        src={require("./../../assets/images/testimonial-image-03.png")}
-                        alt="Testimonial 03"
-                        width={56}
-                        height={56}
-                      />
-                    </div>
-                    <div className="testimonial-item-content">
-                      <p className="text-sm mb-0">
-                        — Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur.
-                        Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum
-                        cillum dolore eu fugiat.
-                      </p>
-                    </div>
-                    <div className="testimonial-item-footer h6 mt-32 mb-0 pt-24 has-top-divider">
-                      <span className="testimonial-item-name">
-                        Erika Muliawan
-                      </span>
-                      <span className="text-color-low"> - </span>
-                      <span className="testimonial-item-link text-xs">
-                        <a href="#0">AppName</a>
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </div>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
